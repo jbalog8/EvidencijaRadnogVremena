b01a7bbd32b65266bff21e85688a73f9ac7833ac
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-03 23:21:58 +0100

    Creating Evidencija Util
    Creating class Evidencija Util for my final project in Edunova School

---
+++ src/main/java/evidencija/util/EvidencijaUtil.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evidencija.util;
+
+import evidencija.model.Operater;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import us.codecraft.xsoup.Xsoup;
+
+/**
+ *
+ * @author jbalog8
+ */
+public class EvidencijaUtil {
+    public static Operater operater;
+    public static final String NAZIV_APP="Edunova app";
+    
+    public static String getNaslov(String naslov){
+        if(EvidencijaUtil.operater==null){
+            return EvidencijaUtil.NAZIV_APP + " " + naslov;
+        }
+        return EvidencijaUtil.NAZIV_APP + " " + naslov +" " + EvidencijaUtil.operater.getIme() + " " + 
+                EvidencijaUtil.operater.getPrezime() + " - " + EvidencijaUtil.operater.getUloga();
+    }
+
+    public static String generirajOib() {
+        // DZ
+        // Umjesto spajanja na web stranicu napraviti generiranje u java programskom jeziku
+        // https://regos.hr/app/uploads/2018/07/KONTROLA-OIB-a.pdf
+        try {
+            URL url = new URL("http://oib.itcentrala.com/oib-generator/");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            url.openStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            in.close();
+            Document d = Jsoup.parse(sb.toString());
+            return Xsoup.compile("/html/body/div[1]/div[1]/text()").evaluate(d).get();
+        } catch (Exception e) {
+        }
+        return "";
+    }
+    
+}
