e084653c3357c0972a0d19564d5f3171b31aecd1
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-04 00:40:03 +0100

    Creating class VrstaRada
    Creating class VrstaRada for my final project in Edunova School

---
+++ src/main/java/evidencija/model/VrstaRada.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evidencija.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jbalog8
+ */
+@Entity
+public class VrstaRada extends Entitet{
+    private String nazivRada;
+
+    public String getNaziv() {
+        return nazivRada;
+    }
+
+    public void setNaziv(String naziv) {
+        this.nazivRada = naziv;
+    }
+    
+    
+}
