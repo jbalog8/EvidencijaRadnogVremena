8ee87b3751f35cd9843f2a738e2bead8db601166
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-13 18:21:45 +0100

    Creating class Obrada Odjel
    Creating class Obrada Odjel for my final project in Edunova School

---
+++ src/main/java/evidencija/controller/ObradaOdjel.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evidencija.controller;
+
+import evidencija.model.Odjel;
+import evidencija.util.EvidencijaException;
+import java.util.List;
+
+/**
+ *
+ * @author jbalog8
+ */
+public class ObradaOdjel extends Obrada<Odjel>{
+
+    @Override
+    public List<Odjel> read() {
+        return session.createQuery("from Odjel").list();
+    }
+    
+
+    @Override
+    protected void kontrolaCreate() throws EvidencijaException {
+        kontrolaNaziv();
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws EvidencijaException {
+    kontrolaNaziv();
+    }
+
+    @Override
+    protected void kontrolaDelete() throws EvidencijaException {
+        
+       
+    }
+
+    private void kontrolaNaziv() throws EvidencijaException{
+       if(entitet.getNaziv()==null || entitet.getNaziv().trim().isEmpty()){
+            throw new EvidencijaException("Naziv odjela obavezan");
+         }
+          if(entitet.getNaziv().trim().length()>50){
+            throw new EvidencijaException("Naziv odjela ne smije biti duži od 50 znakova");
+        }
+    }
+    
+}
