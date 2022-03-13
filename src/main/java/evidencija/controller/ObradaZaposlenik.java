7b3fb783b9ac2f494f9841177d0db940111f6d57
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-13 18:22:13 +0100

    Creating Obrada Zaposlenik
    Creating class Obrada Zaposlenik for my final project in Edunova School

---
+++ src/main/java/evidencija/controller/ObradaZaposlenik.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evidencija.controller;
+
+import evidencija.model.Zaposlenik;
+import evidencija.util.EvidencijaException;
+import java.util.List;
+
+/**
+ *
+ * @author jbalog8
+ */
+public class ObradaZaposlenik extends ObradaOsoba<Zaposlenik>{
+
+    @Override
+    public List<Zaposlenik> read() {
+        return session.createQuery("from Zaposlenik").list();
+    }
+
+    @Override
+    protected void kontrolaCreate() throws EvidencijaException {
+        super.kontrolaCreate();
+        kontrolaBrKartice();
+    }
+
+    private void kontrolaBrKartice() throws EvidencijaException{
+        if(entitet.getBrKartice() == null && entitet.getBrKartice().length() < 13){
+            throw new EvidencijaException("Broj kartice ne moze biti 0 i ne moze biti manje od 13 znamaneki");
+        }
+    }
+
+    @Override
+    protected void kontrolaDelete() throws EvidencijaException {
+        super.kontrolaDelete();
+        
+        if(entitet.getOdjel()!= null){
+            throw new EvidencijaException("Zaposlenik se nalazi na jednom odjelu");
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
