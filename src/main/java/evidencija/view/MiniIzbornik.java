f6640fb426fa124c7e4c8b69993b0a12bb107f42
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-04-04 15:29:24 +0200

    Updating jFrame Mini Izbornik

--- src/main/java/evidencija/view/MiniIzbornik.java
+++ src/main/java/evidencija/view/MiniIzbornik.java
@@ -28,6 +28,7 @@ public class MiniIzbornik extends javax.swing.JFrame {
 
         btnAdministarcija = new javax.swing.JButton();
         btnPocetak = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -46,6 +47,14 @@ public class MiniIzbornik extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
+        jLabel1.setText("X");
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -56,15 +65,21 @@ public class MiniIzbornik extends javax.swing.JFrame {
                     .addComponent(btnPocetak, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnAdministarcija, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(101, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(71, 71, 71)
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(42, 42, 42)
                 .addComponent(btnAdministarcija, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(42, 42, 42)
                 .addComponent(btnPocetak, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(107, Short.MAX_VALUE))
+                .addContainerGap(91, Short.MAX_VALUE))
         );
 
         pack();
@@ -72,20 +87,26 @@ public class MiniIzbornik extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAdministarcijaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAdministarcijaActionPerformed
-       new Autorizacija().setVisible(true);
+        new Autorizacija().setVisible(true);
     }//GEN-LAST:event_btnAdministarcijaActionPerformed
 
     private void btnPocetakActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPocetakActionPerformed
-       new AutorizacijaZaposlenik().setVisible(true);
+        new AutorizacijaZaposlenik().setVisible(true);
     }//GEN-LAST:event_btnPocetakActionPerformed
+
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        if (evt.getClickCount() == 2) {
+            System.exit(0);
+        }
+    }//GEN-LAST:event_jLabel1MouseClicked
 
     /**
      * @param args the command line arguments
      */
-   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdministarcija;
     private javax.swing.JButton btnPocetak;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
