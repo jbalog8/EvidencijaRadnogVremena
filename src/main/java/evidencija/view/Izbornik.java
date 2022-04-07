bc83b424f67a4b207b07faedfdd337c07990cabd
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-04-07 23:43:37 +0200

    Updating Izbornik

--- src/main/java/evidencija/view/Izbornik.java
+++ src/main/java/evidencija/view/Izbornik.java
@@ -147,7 +147,7 @@ public class Izbornik extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        new EvidencijaProzor().setVisible(true);
+        new EvidencijaZaposlenik().setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
 
