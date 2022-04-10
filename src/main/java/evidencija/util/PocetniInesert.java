670a1349d48c09d9bd439f7434f453f99cbca664
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-04-10 22:54:20 +0200

    Updating Pocentni Inserti

--- src/main/java/evidencija/util/PocetniInesert.java
+++ src/main/java/evidencija/util/PocetniInesert.java
@@ -11,6 +11,7 @@ import evidencija.model.Operater;
 import evidencija.model.VrstaRada;
 import evidencija.model.Zaposlenik;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import org.hibernate.Session;
@@ -31,9 +32,9 @@ public class PocetniInesert {
         List<Odjel> odjeli = unosOdjela(faker, session); //pitanje ne radi
         List<VrstaRada> vrstaRada = unesiVrstuRada(faker, session); //pitanje ne radi
 
-        unosZaposlenika(odjeli, faker, session);
+        //unosZaposlenika(odjeli, faker, session);
 
-        List<Zaposlenik> zaposlenici = new ArrayList<>();
+        List<Zaposlenik> zaposlenici = unosZaposlenika(odjeli, faker, session);//new ArrayList<>();
         unesiEvidenciju(zaposlenici, vrstaRada, faker, session);
 
         //unosOdjela(faker, session);
@@ -52,16 +53,19 @@ public class PocetniInesert {
 
     }
 
-    private static void unosZaposlenika(List<Odjel> odjeli, Faker faker, Session session) {
+    private static List<Zaposlenik> unosZaposlenika(List<Odjel> odjeli, Faker faker, Session session) {
+        
+        List<Zaposlenik> zaposlenici = new ArrayList<>();
         Zaposlenik z;
         z = new Zaposlenik();
         z.setIme("Josip");
         z.setPrezime("Balog");
         z.setEmail("jbalog8@fakecompay.com");
-        // z.setOdjel(z.getOdjel());
+        z.setOdjel((odjeli.get(0)));
         z.setOib(EvidencijaUtil.generirajOib());
         z.setBrKartice(EvidencijaUtil.generirajKarticu());
         session.save(z);
+        zaposlenici.add((z));
 
         for (int i = 0; i < 20; i++) {
             z = new Zaposlenik();
@@ -75,9 +79,11 @@ public class PocetniInesert {
 
             z.setOdjel(odjeli.get((int) ((Math.random() * (4 - 1)) + 1)));
             session.save(z);
-
+            zaposlenici.add((z)); 
         }
+        
 
+        return zaposlenici;
     }
 
     private static List<Odjel> unosOdjela(Faker faker, Session session) {
@@ -101,16 +107,53 @@ public class PocetniInesert {
         for (int i = 0; i < zaposlenici.size(); i++) {
             z = zaposlenici.get(i);
 
-            for (int j = 0; j < 50; i++) {
+            for (int j = 0; j < 10; j++) {
                 e = new Evidencija();
                 e.setZaposlenik(z);
-                e.setDatumPocetak(null);
-                vrsteRada = new ArrayList<>();
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.MONTH, 0); // mjeseci kreću od nule
+                c.set(Calendar.MONTH, 1);
+                c.set(Calendar.MONTH, 2);
+                c.set(Calendar.DAY_OF_MONTH, slucajniBroj(27, 1));
+                c.set(Calendar.YEAR, 2022);
+                c.set(Calendar.HOUR_OF_DAY, slucajniBroj(9, 7));
+                c.set(Calendar.MINUTE, slucajniBroj(59, 1));
+                c.set(Calendar.SECOND, slucajniBroj(59, 1));
+
+                e.setDatumPocetak(c.getTime());
+                c.add(Calendar.MINUTE, slucajniBroj((8 * 60) + 10, (7 * 60) + 50));
+                e.setDatumKraj(c.getTime());
                 e.setVrstaRada(vrsteRada.get((int) ((Math.random() * (3 - 1)) + 1)));
                 session.save(e);
 
             }
         }
+    }
+
+    /*
+    private static void radSDatumima(){
+         Calendar c = Calendar.getInstance();
+        for(int i=0;i<10;i++){
+            c.set(Calendar.MONTH, 5); // mjeseci kreću od nule
+        c.set(Calendar.DAY_OF_MONTH, slucajniBroj(27, 1));
+        c.set(Calendar.YEAR, 2022);
+        c.set(Calendar.HOUR_OF_DAY,  slucajniBroj(9, 7));
+        c.set(Calendar.MINUTE,  slucajniBroj(59, 1));
+        c.set(Calendar.SECOND, slucajniBroj(59, 1) );
+        System.out.println("Početak: " + c.getTime());
+        if(slucajniBroj(19, 1)>5){
+            c.add(Calendar.MINUTE, slucajniBroj((8*60) + 10, (7 * 60) + 50));
+            System.out.println("Kraj: " + c.getTime());
+        }else{
+            //postaviš završni datum na null (ne moraš ništa raditi)
+        }
+            System.out.println("-------------");
+        
+        }
+    }
+     */
+    private static int slucajniBroj(int max, int min) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 
     private static List<VrstaRada> unesiVrstuRada(Faker faker, Session session) {
