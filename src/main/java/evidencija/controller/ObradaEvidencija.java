920738cf1b8bf81ce594e6add5587e9f11d20244
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-04-10 22:53:15 +0200

    Updating Obrada Evidencija

--- src/main/java/evidencija/controller/ObradaEvidencija.java
+++ src/main/java/evidencija/controller/ObradaEvidencija.java
@@ -7,6 +7,7 @@ package evidencija.controller;
 import evidencija.model.Evidencija;
 import evidencija.model.Zaposlenik;
 import evidencija.util.EvidencijaException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,50 +20,58 @@ public class ObradaEvidencija extends Obrada<Evidencija> {
     public List<Evidencija> read() {
         return session.createQuery("from Evidencija").list();
     }
-     public List<Evidencija> read(String uvjet) {
-        return session.createQuery("from Evidencija e " 
-                + "where concat(e.datumPocetak,' ', e.datumKraj)"
-                + "like : uvjet order by e.datumPocetak, e.datumKraj")
-                .setParameter("uvjet", "%" + uvjet + "%")
+
+    public List<Evidencija> read(String uvjet) {
+
+        return session.createQuery("from Evidencija  "
+                + " where (zaposlenik_sifra = :zaposlenik_sifra)"
+                + " order by  datumPocetak, datumKraj")
+                .setParameter("zaposlenik_sifra", uvjet)
                 .setMaxResults(50)
                 .list();
+
+}
+
+    public Evidencija getZaposlenik(Zaposlenik zaposlenik) {
+        try {
+            return (Evidencija) session.createQuery("from Evidencija where zaposlenik=:zaposlenik")
+                    .setParameter("zaposlenik", zaposlenik).setMaxResults(50);
+        } catch (Exception e) {
+            return null;
+        }
+
     }
-    
-    
 
     @Override
     protected void kontrolaCreate() throws EvidencijaException {
-        
+
     }
 
     @Override
     protected void kontrolaUpdate() throws EvidencijaException {
-      
+
     }
 
     @Override
     protected void kontrolaDelete() throws EvidencijaException {
-        if (entitet.getZaposlenik() != null) {
-            throw new EvidencijaException("Evidenciju ne mozeš obrisati zato što pripada zaposleniku");
-        }
+//        if (entitet.getZaposlenik() != null) {
+//            throw new EvidencijaException("Evidenciju ne mozeš obrisati zato što pripada zaposleniku");
+//        }
     }
 
-    public Zaposlenik getZaposlenik(String ime){
-        if(entitet.getZaposlenik() == null){
-        try {
-            return (Zaposlenik) session.createQuery("from Zaposlenik where ime=:ime")
-                    .setParameter("ime", ime).getSingleResult();
-            
-        } catch (Exception e) {
-            
-        
-        
-    }
+    public Zaposlenik getZaposlenik(String ime) {
+        if (entitet.getZaposlenik() == null) {
+            try {
+               return (Zaposlenik) session.createQuery("from Zaposlenik where ime=:ime")
+                        .setParameter("ime", ime).getSingleResult();
+
+            } catch (Exception e) {
+
+            }
 
         }
-    
-return null;
-}
-    
-}
 
+        return null;
+    }
+
+}
