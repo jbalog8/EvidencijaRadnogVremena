6272f13530dfb44319ecdf2faee8ac01b1baec62
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-04-20 12:06:34 +0200

    Updating jFrame Autorizacija Zaposlenika

--- src/main/java/evidencija/view/AutorizacijaZaposlenik.java
+++ src/main/java/evidencija/view/AutorizacijaZaposlenik.java
@@ -5,12 +5,15 @@
 package evidencija.view;
 
 import evidencija.controller.ObradaEvidencija;
+import evidencija.controller.ObradaVrstaRada;
 import evidencija.controller.ObradaZaposlenik;
 import evidencija.model.Evidencija;
+import evidencija.model.VrstaRada;
 import evidencija.model.Zaposlenik;
 import evidencija.util.HibernateUtil;
 import java.util.Date;
 import java.util.List;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
@@ -25,6 +28,7 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
 
     private ObradaZaposlenik obrada;
     private ObradaEvidencija obradaEvidencija;
+   
 
     /**
      * Creates new form AutorizacijaZaposlenik
@@ -34,7 +38,10 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
 
         obrada = new ObradaZaposlenik();
         obradaEvidencija = new ObradaEvidencija();
-
+        ucitajVrstuRada();
+        
+        
+         
         txtBrojKartice.getText();
 
         txtBrojKartice.getDocument().addDocumentListener(new DocumentListener() {
@@ -61,9 +68,8 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
             return;
         }
          Zaposlenik zaposlenik = obrada.getZaposlenik(txtBrojKartice.getText());
-         
         if(zaposlenik==null){
-            JOptionPane.showMessageDialog(null, "Ne postoji zaposlenik/ca");
+            JOptionPane.showMessageDialog(null, "Ne postoji zaposlenik");
             return;
         }
         if(zaposlenik.getAktivan() == false){
@@ -82,6 +88,7 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
             try {
                
                 obradaEvidencija.create();
+                preuzmiVrijednost();
                 JOptionPane.showMessageDialog(null, "Uspješno ste se prijavili " + e.getZaposlenik());
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, "greška dodavanje novi");
@@ -100,7 +107,7 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
                 obradaEvidencija.update();
                 JOptionPane.showMessageDialog(null, "Uspješno ste se odjavili " + evidencija.getZaposlenik());
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "nije dobro");
+                JOptionPane.showMessageDialog(null, "Nije dobro");
                 return;
             }
         }
@@ -135,6 +142,7 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         btnPRIJAVA = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        cmbVrstaRada = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setUndecorated(true);
@@ -173,29 +181,36 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(28, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGap(0, 88, Short.MAX_VALUE)
                         .addComponent(btnPRIJAVA)
                         .addGap(73, 73, 73))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(cmbVrstaRada, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+                        .addGap(97, 97, 97))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmbVrstaRada, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtBrojKartice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -225,10 +240,29 @@ public class AutorizacijaZaposlenik extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnPRIJAVA;
+    private javax.swing.JComboBox<VrstaRada> cmbVrstaRada;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField txtBrojKartice;
     // End of variables declaration//GEN-END:variables
 
+    private void ucitajVrstuRada() {
+        DefaultComboBoxModel<VrstaRada> ms = new DefaultComboBoxModel<>();
+        VrstaRada vr = new VrstaRada();
+        vr.setSifra(Long.valueOf(0));
+        vr.setNaziv("-");
+        ms.addElement(vr);
+        new ObradaVrstaRada().read().forEach(s -> {
+            ms.addElement(s);
+        });
+        cmbVrstaRada.setModel(ms);
+    }
+    
+    private void preuzmiVrijednost(){
+        var e = obradaEvidencija.getEntitet();
+        e.setVrstaRada((VrstaRada)cmbVrstaRada.getSelectedItem());
+    }
+    
+   
 }
